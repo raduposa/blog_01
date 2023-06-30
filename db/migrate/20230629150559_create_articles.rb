bfae6b5f0077eac792fb718c2e8e762a708af0ea
Radu <posaradu.cp@gmail.com>
2023-06-30 14:43:23 +0300

    Fourth commit

--- db/migrate/20230629150559_create_articles.rb
+++ db/migrate/20230629150559_create_articles.rb
@@ -2,7 +2,8 @@ class CreateArticles < ActiveRecord::Migration[7.0]
   def change
     create_table :articles do |t|
       t.string :title
-      t.text :body
+      t.text :description
+      add_column :articles, :description, :string
 
       t.timestamps
     end
